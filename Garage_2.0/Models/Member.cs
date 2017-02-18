42f1ed7ee5c2d996ae1c11731041e01beb15197e
Lena Widegren <widenzia@gmail.com>
2017-02-19 00:26:30 +0100

    Changes in Members for usability and Garage specific design

--- Garage_2.0/Models/Member.cs
+++ Garage_2.0/Models/Member.cs
@@ -9,8 +9,15 @@ namespace Garage_2._0.Models
     public class Member
     {
         public int Id { get; set; }
+
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Display(Name = "Member")]
+        public string FullName { get { return (FirstName + " " + LastName).Trim(); } }
 
         // Navigation property
         public virtual ICollection<Vehicle> Vehicles { get; set; }
